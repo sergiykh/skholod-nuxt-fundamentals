baffb6e4f60fffb63ec7b8db3d6b7e10ce2e3c12
sergiykh <Today31$>
2020-11-07 10:26:08 +0100

    make it better

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <TheNavbar/>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+import TheNavbar from '@/components/TheNavbar'
+export default {
+  components: {
+    TheNavbar
+  }
+}
+</script>
+
+<style>
+  
+</style>
